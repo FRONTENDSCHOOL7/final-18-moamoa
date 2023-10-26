c10ead5178b531b193b78e6ac4c3df8ade26ca7e
timetam <timetam24@naver.com>
2023-10-26 16:44:16 +0900

    Rename: Recoil 폴더 안 파일명 변경 & 파일 삭제

---
+++ moamoa/src/Recoil/followPostAtom.jsx
@@ -0,0 +1,15 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+const followPostAtom = atom({
+  key: 'followPostAtom',
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export default followPostAtom;
+
+// 변수명 PostState을 followPostAtom으로 변경했습니다.
+// 키 값 postState를 followPostAtom으로 변경했습니다.
