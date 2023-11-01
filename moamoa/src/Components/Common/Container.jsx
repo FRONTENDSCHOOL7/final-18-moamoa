58e4fc2d308e874a6d30c1512a77c04c26b1d461
AYFG <sek82468246@gmail.com>
2023-11-02 05:48:14 +0900

    fix: 충돌 해결

--- moamoa/src/Components/Common/Container.jsx
+++ moamoa/src/Components/Common/Container.jsx
@@ -5,7 +5,7 @@ export const Container = styled.div`
   max-width: 390px;
   width: 100%;
   flex: 1;
-  height: 100rem;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   position: relative;
