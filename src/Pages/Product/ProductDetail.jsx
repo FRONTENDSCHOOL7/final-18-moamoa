e143fc1e234ea812856715a29def613efbfd30e7
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-01-20 02:01:02 +0900

    refactor: 축제 및 행사 상세 style 분리

--- src/Pages/Product/ProductDetail.jsx
+++ src/Pages/Product/ProductDetail.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 import Header from '../../Components/Common/Header/Header';
 import AskBtn from '../../Components/Product/AskBtn';
 import ArticleUserProfile from '../../Components/Common/ArticleUserProfile';
 import Footer from '../../Components/Common/Footer';
+import { FestivalContainer, FestivalArticle, Frofile } from './ProductStyle';
 
 import { getProductDetail } from '../../API/Product/ProductAPI';
 import ProductContents from '../../Components/Product/ProductContents';
@@ -39,47 +39,18 @@ export default function ProductDetail() {
   return (
     <>
       {productData && productAuthorInfo && (
-        <Container>
-          <FestivalContainer>
-            <Header type='home' />
-            <FestivalArticle>
-              <Frofile>
-                <ArticleUserProfile userProfileData={userProfileData} />
-                <AskBtn btnData={btnData} />
-              </Frofile>
-              <ProductContents productData={productData} />
-            </FestivalArticle>
-            <Footer />
-          </FestivalContainer>
-        </Container>
+        <FestivalContainer>
+          <Header type='home' />
+          <FestivalArticle>
+            <Frofile>
+              <ArticleUserProfile userProfileData={userProfileData} />
+              <AskBtn btnData={btnData} />
+            </Frofile>
+            <ProductContents productData={productData} />
+          </FestivalArticle>
+          <Footer />
+        </FestivalContainer>
       )}
     </>
   );
 }
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #fff9e4;
-`;
-const FestivalContainer = styled.div`
-  max-width: 39rem;
-  width: 100%;
-  height: 100%;
-  margin: auto;
-
-  background-color: #ffffff;
-  overflow: hidden;
-`;
-const FestivalArticle = styled.article`
-  margin-top: 4.8rem;
-  margin-bottom: 7rem;
-`;
-const Frofile = styled.div`
-  height: 4.2rem;
-  padding: 0.7rem 1.2rem;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
