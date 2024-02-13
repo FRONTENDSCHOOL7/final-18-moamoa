d04c8d43cb3f1203e4bc3b99f6743a7969fff1c9
AYFG <sek82468246@gmail.com>
2024-02-13 22:10:06 +0900

    검색 페이지의 input과 placeholder 크기 확대,채팅리스트 가운데 정렬,팔로우 페이지 가운데 정렬과 팔로우 버튼 사이즈 축소,축제 리스트 최하단 로고 제거,px 사용한 곳에 rem 으로 변경 진행

--- src/Pages/Chat/ChatStyle.js
+++ src/Pages/Chat/ChatStyle.js
@@ -189,7 +189,7 @@ export const ChatListWrap = styled.ul`
   gap: 16px;
   margin-top: 48px;
   @media (min-width: 768px) {
-    width: 480px;
+    width: 48rem;
     padding: 0;
     margin-top: 80px;
     margin-inline: auto;
