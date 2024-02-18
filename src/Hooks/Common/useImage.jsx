e0a6a9d63b28bb84ef0894f804940cb2e6a7fdad
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-02-19 04:41:26 +0900

    fix: 충돌 코드 제거

--- src/Hooks/Common/useImage.jsx
+++ src/Hooks/Common/useImage.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import imageCompression from 'browser-image-compression';
 
 export const useImage = (initialImg) => {
   const [showImgModal, setShowImgModal] = useState(false);
@@ -58,22 +57,8 @@ export const useImage = (initialImg) => {
           imageUrl: resizedImageUrl || '', // 새로운 이미지 설정
         }));
       });
-
       reader.readAsDataURL(e.target.files[0]);
-      const compFile = e.target.files[0];
-      const compOptions = {
-        // maxSizeMB: 0.5,
-        maxWidthOrHeight: 650,
-        useWebWorker: true
-      };
-      const compressedFile =  await imageCompression(compFile, compOptions);
-      // setCompImg(compressedFile);
-      const promise = imageCompression(compressedFile);
-      await promise.then((result) => {
-        setImgData(result);
-        reader.readAsDataURL(result);
-        setShowImgModal(true);
-      })
+      setShowImgModal(true);
     }
   }
 
@@ -88,8 +73,6 @@ export const useImage = (initialImg) => {
 
   // 모달에서 크롭한 이미지 저장
   const setCroppedImageFor = (crop, zoom, croppedImageUrl) => {
-
-    
     const newImage = { ...imgData, croppedImageUrl, crop, zoom };
     setImgData(newImage);
     setShowImgModal(false);
